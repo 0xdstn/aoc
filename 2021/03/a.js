2e1c681bc0669eaa6bfec9a62e75bcaf3145e184
Dustin <0xdstn@protonmail.com>
2021-12-02 22:15:37 -0800

    Added 2021-03

---
+++ 2021/03/a.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+console.log('')
+console.log('[*] AoC 2021-03-A')
+console.log('')
+
+require('../common/readInput.js').get('input.txt', (data) => {
+  let ones = [0,0,0,0,0,0,0,0,0,0,0,0]
+  let zeros = [0,0,0,0,0,0,0,0,0,0,0,0]
+  
+  data.forEach(x => {
+    for(let y in x) {
+      if(x[y] == '1') {
+        ones[y]++
+      } else {
+        zeros[y]++
+      }
+    }
+  })
+
+  let gammaBinary = ''
+  let epsilonBinary = ''
+
+  for(let z in ones) {
+    if(ones[z] > zeros[z]) {
+      gammaBinary += '1'
+      epsilonBinary += '0'
+    } else {
+      gammaBinary += '0'
+      epsilonBinary += '1'
+    }
+  }
+
+  let gamma = parseInt(gammaBinary,2)
+  let epsilon = parseInt(epsilonBinary,2)
+
+  console.log('')
+  console.log('Answer: ', gamma*epsilon)
+})
+
---
+++ 2021/03/b.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+console.log('')
+console.log('[*] AoC 2021-03-B')
+console.log('')
+
+require('../common/readInput.js').get('input.txt', (data) => {
+  
+  origData = [...data]
+
+  index = 0
+  indexMax = data[0].length
+  while(data.length > 1 && index < indexMax) {
+    let ones = 0
+    let zeros = 0
+
+    data.forEach(x => {
+      if(x[index] == '1') {
+        ones++
+      } else {
+        zeros++
+      }
+    })
+
+    let val = ''
+
+    if(ones > zeros) {
+      val = '1'
+    }
+    else if(ones < zeros) {
+      val = '0'
+    } else {
+      val = '1'
+    }
+
+    for(let y = data.length-1; y >= 0; y--) {
+      if(data[y][index] !== val) {
+        data.splice(y, 1)
+      }
+    }
+    index++
+  }
+
+  oxy = parseInt(data[0], 2)
+
+  data = [...origData]
+
+  index = 0
+  indexMax = data[0].length
+  while(data.length > 1 && index < indexMax) {
+    let ones = 0
+    let zeros = 0
+
+    data.forEach(x => {
+      if(x[index] == '1') {
+        ones++
+      } else {
+        zeros++
+      }
+    })
+
+    let val = ''
+
+    if(ones > zeros) {
+      val = '0'
+    }
+    else if(ones < zeros) {
+      val = '1'
+    } else {
+      val = '0'
+    }
+
+    for(let y = data.length-1; y >= 0; y--) {
+      if(data[y][index] !== val) {
+        data.splice(y, 1)
+      }
+    }
+    index++
+  }
+
+  co2 = parseInt(data[0], 2)
+
+  console.log('')
+  console.log('Answer: ', oxy*co2)
+})
+
