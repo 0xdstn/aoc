720f5ec727a6e92416071bc57ad11925e86204c3
Dustin <0xdstn@protonmail.com>
2022-12-01 17:21:53 +0000

    2022-01

--- 2022/01/a.js
+++ 2022/01/a.js
@@ -5,9 +5,21 @@ console.log('[*] AoC 2022-01-A')
 console.log('')
 
 require('../common/readInput.js').get('input.txt', (data) => {
-	
-  var answer;
-  
-  console.log('')
-  console.log('Answer:', answer)
+
+    var answer = 0
+    var cur = 0
+
+    data.forEach(x => {
+        if(x == '') {
+            if(answer < cur) answer = cur
+            cur = 0
+        } else {
+            cur += parseInt(x)
+        }
+    })
+
+    if(answer < cur) answer = cur
+
+    console.log('')
+    console.log('Answer:', answer)
 })
--- 2022/01/b.js
+++ 2022/01/b.js
@@ -5,9 +5,28 @@ console.log('[*] AoC 2022-01-B')
 console.log('')
 
 require('../common/readInput.js').get('input.txt', (data) => {
-	
-  var answer;
-  
-  console.log('')
-  console.log('Answer:', answer)
+
+    var answer = 0
+    var cur = 0
+    var totals = []
+
+    data.forEach(x => {
+        if(x == '') {
+            if(answer < cur) totals.push(cur)
+            cur = 0
+        } else {
+            cur += parseInt(x)
+        }
+    })
+
+    totals.push(cur)
+
+    totals.sort((a, b) => {
+          return b-a
+    })
+
+    answer = totals[0] + totals[1] + totals[2]
+
+    console.log('')
+    console.log('Answer:', answer)
 })
